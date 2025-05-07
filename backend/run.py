562e507f22b8927308e9be3875dd47587057aea9
Efraim Hernandez <efras15@gmail.com>
2025-05-07 16:26:19 -0600

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     try:
-        uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+        uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
     except Exception as e:
         print(f"Error starting the application: {e}")
         sys.exit(1)
